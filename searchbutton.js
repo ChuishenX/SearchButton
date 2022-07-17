43cfc9507460786b5a569df3c926ba8db0317e58
cysn <92655031+Fgaoxing@users.noreply.github.com>
2022-07-18 00:16:57 +0800

    Update searchbutton.js

--- searchbutton.js
+++ searchbutton.js
@@ -5,26 +5,55 @@ function searching() {
         searches[i].className = 'search';
         var type = searches[i].getAttribute('type');
         var content = searches[i].getAttribute('content')
-        if (type == 'baidu') {
-            searches[i].innerHTML = '<div><button class="search" style="background-color: blue;" onclick="window.open('+'\"https://www.baidu.com/s?wd='+ content +'\')" title="请点击使用百度搜索该关键词">搜索'+content+'</button></div>';
-        } else if (type == 'google') {
-            searches[i].innerHTML = '<div><button class="search" style="background-color: green;" onclick="window.open('+'\'https://www.google.com/search?q='+ content +'\')" title="请点击使用谷歌搜索该关键词">搜索'+content+'</button></div>';
-        } else if (type == 'sogou') {
-            searches[i].innerHTML = '<div><button class="search" style="background-color: pink;" onclick="window.open('+'\'https://www.sogou.com/web?query='+ content +'\')" title="请点击使用搜狗搜索该关键词">搜索'+content+'</button></div>';
-        } else if (type == 'S360') {
-            searches[i].innerHTML = '<div><button class="search" style="background-color: lime;" onclick="window.open('+'\'https://www.so.com/s?q='+ content +'\')" title="请点击使用360搜索该关键词">搜索'+content+'</button></div>';
-        } else if (type == 'bing') {
-            searches[i].innerHTML = '<div><button class="search" style="background-color: gold;" onclick="window.open('+'\'https://www.bing.com/search?q='+ content +'\')" title="请点击使用必应搜索该关键词">搜索'+content+'</button></div>';
-        } else if (type == 'github') {
-            searches[i].innerHTML = '<div><button class="search" style="background-color: black;" onclick="window.open('+'\'https://github.com/search?q='+ content +'\')" title="请点击使用GitHub搜索该关键词">搜索'+content+'</button></div>';
-        } else if (type == 'yandex') {
-            searches[i].innerHTML = '<div><button class="search" style="background-color: red;" onclick="window.open('+'\'https://yandex.com/search/?text='+ content +'\')" title="请点击使用Yandex搜索该关键词">搜索'+content+'</button></div>';
+        searchesJson={
+            baidu: {
+                name: '百度',
+                color: 'blue',
+                url: 'https://www.baidu.com/s?wd='
+            },
+            google: {
+                name: '谷歌',
+                color: 'green',
+                url: 'https://www.google.com/search?q='
+            },
+            sogou: {
+                name: '搜狗',
+                color: 'pink',
+                url: 'https://www.sogou.com/web?query='
+            },
+            s360: {
+                name: '360',
+                color: 'lime',
+                url: 'https://www.so.com/s?q='
+            },
+            bing: {
+                name: '必应',
+                color: 'gold',
+                url: 'https://www.baidu.com/s?wd='
+            },
+            github: {
+                name: 'github',
+                color: 'black',
+                url: 'https://github.com/search?q='
+            },
+            yandex: {
+                name: 'yandex',
+                color: 'red',
+                url: 'https://yandex.com/search/?text='
+            }
         }
+        searches[i].innerHTML = '<div><button class="search" style="background-color: '+searchesJson[type].color+';" onclick="window.open(`'+searchesJson[type].url + content +'`)" title="请点击使用'+searchesJson[type].name+'搜索该关键词">搜索'+content+'</button></div>';
     }
 }
 //页面加载后运行
-window.onload = function () {
-    searching();
+if (window.onload) {
+    window.onload += function () {
+        searching()
+    }
+} else {
+    window.onload = function () {
+        searching()
+    }
 }
 
 // <search type="baidu" content="1234"></search>
